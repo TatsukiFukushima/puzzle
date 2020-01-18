c93dd115839fa6dc7f128b68fcf36d4b6fb5e7c1
TatsukiFukushima <fukushimatatsuki@Fukushima-no-MacBook-Air.local>
2020-01-18 15:09:01 +0900

    だいたい２倍に高速化

--- puzzle.go
+++ puzzle.go
@@ -5,53 +5,51 @@ import (
 	"strconv"
 )
 
-const depth int = 22
+const depth int = 24
 var	rate float32 = 0.0
 
 var (
 	moves     [depth+1]string
-	maxPoint  int = 0
-	maxPoint2 int = 0
-	maxPoint3 int = 0
-	maxMoves  [depth+1]string
-	maxMoves2 [depth+1]string
-	maxMoves3 [depth+1]string
+	minPoint  int = 30
+	minPoint2 int = 30
+	minPoint3 int = 30
+	minMoves  [depth+1]string
+	minMoves2 [depth+1]string
+	minMoves3 [depth+1]string
 )
 
 func main() {
 	board := Board {
-		{9, 9, 9, 9, 9, 9, 9, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 9, 9, 9, 9, 9, 9, 9},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
 	}
 
 	// 盤面を入力
-	for i := 1; i < 6; i++ {
+	for i := 0; i < 5; i++ {
 		var b1, b2, b3, b4, b5, b6 string
 		fmt.Print("> ")
 		fmt.Scan(&b1, &b2, &b3, &b4, &b5, &b6)
 
-		board[i][1], _ = strconv.Atoi(b1)
-		board[i][2], _ = strconv.Atoi(b2)
-		board[i][3], _ = strconv.Atoi(b3)
-		board[i][4], _ = strconv.Atoi(b4)
-		board[i][5], _ = strconv.Atoi(b5)
-		board[i][6], _ = strconv.Atoi(b6)
+		board[i][0], _ = strconv.Atoi(b1)
+		board[i][1], _ = strconv.Atoi(b2)
+		board[i][2], _ = strconv.Atoi(b3)
+		board[i][3], _ = strconv.Atoi(b4)
+		board[i][4], _ = strconv.Atoi(b5)
+		board[i][5], _ = strconv.Atoi(b6)
 	}
 	fmt.Println("")
 	board.print()
 
 	// ルートを探索
-	for i := 1; i < 6; i++ {
-		for j := 1; j < 7; j++ {
-			moves[0] = strconv.Itoa(j) + strconv.Itoa(i)
+	for i := 0; i < 5; i++ {
+		for j := 0; j < 6; j++ {
+			moves[0] = strconv.Itoa(j+1) + strconv.Itoa(i+1)
 			move(1, board, j, i)
 			rate += 10/3.0
-			log := strconv.Itoa(int(rate)) + "% 最大値:" + strconv.Itoa(maxPoint)
+			log := strconv.Itoa(int(rate)) + "% 最大値:" + strconv.Itoa(30 - minPoint)
 			fmt.Printf("\r%s", log)
 		}
 	}
@@ -59,16 +57,16 @@ func main() {
 	fmt.Printf("\r")
 	fmt.Println("---------------------")
 	fmt.Println("候補１:")
-	fmt.Println(maxMoves)
-	fmt.Println("消える数: " + strconv.Itoa(maxPoint))
+	fmt.Println(minMoves)
+	fmt.Println("消える数: " + strconv.Itoa(30 - minPoint))
 	fmt.Println("")
 	fmt.Println("候補２:")
-	fmt.Println(maxMoves2)
-	fmt.Println("消える数: " + strconv.Itoa(maxPoint2))
+	fmt.Println(minMoves2)
+	fmt.Println("消える数: " + strconv.Itoa(30 - minPoint2))
 	fmt.Println("")
 	fmt.Println("候補３:")
-	fmt.Println(maxMoves3)
-	fmt.Println("消える数: " + strconv.Itoa(maxPoint3))
+	fmt.Println(minMoves3)
+	fmt.Println("消える数: " + strconv.Itoa(30 - minPoint3))
 }
 
 // Board 盤面
@@ -80,41 +78,41 @@ func move(n int, board Board, x int, y int) {
 	// 指定の手数以上になった場合、ポイントを算出して最高得点だったらmovesを記録。
 	if n > depth {
 		point = calcPoint(board)
-		if point > maxPoint {
-			maxPoint = point
-			maxMoves = moves
-			log := strconv.Itoa(int(rate)) + "% 最大値:" + strconv.Itoa(maxPoint)
+		if point < minPoint {
+			minPoint = point
+			minMoves = moves
+			log := strconv.Itoa(int(rate)) + "% 最大値:" + strconv.Itoa(30 - minPoint)
 			fmt.Printf("\r%s", log)
-		} else if point > maxPoint2 {
-			maxPoint2 = point
-			maxMoves2 = moves
-		} else if point > maxPoint3 {
-			maxPoint3 = point
-			maxMoves3 = moves
+		} else if point < minPoint2 {
+			minPoint2 = point
+			minMoves2 = moves
+		} else if point < minPoint3 {
+			minPoint3 = point
+			minMoves3 = moves
 		}
 		return
 	} else if n == 9 {
-		// 枝刈り。8回移動して6ポイント以下のルートは探索しない。
+		// 枝刈り。8回移動して24ポイントより大きいルートは探索しない。
 		point = calcPoint(board)
-		if point < 6 {
+		if point > 24 {
 			return
 		}
 	} else if n == 13 {
-		// 枝刈り2。12回移動して9ポイント以下のルートは探索しない。
+		// 枝刈り2。12回移動して21ポイントより大きいルートは探索しない。
 		point = calcPoint(board)
-		if point < 9 {
+		if point > 21 {
 			return
 		}
 	} else if n == 17 {
-		// 枝刈り3。16回移動して12ポイント以下のルートは探索しない。
+		// 枝刈り3。16回移動して18ポイントより大きいルートは探索しない。
 		point = calcPoint(board)
-		if point < 12 {
+		if point > 18 {
 			return
 		}
 	} else if n == 21 {
-		// 枝刈り4。20回移動して12ポイント以下のルートは探索しない。
+		// 枝刈り4。20回移動して18ポイントより大きいルートは探索しない。
 		point = calcPoint(board)
-		if point < 12 {
+		if point > 18 {
 			return
 		}
 	} else if n > depth - 3 {
@@ -122,65 +120,72 @@ func move(n int, board Board, x int, y int) {
 		if point == 0 {
 			point = calcPoint(board)
 		}
-		if point > maxPoint {
-			maxPoint = point
-			maxMoves = moves
-		} else if point > maxPoint2 {
-			maxPoint2 = point
-			maxMoves2 = moves
-		} else if point > maxPoint3 {
-			maxPoint3 = point
-			maxMoves3 = moves
+		if point < minPoint {
+			minPoint = point
+			minMoves = moves
+		} else if point < minPoint2 {
+			minPoint2 = point
+			minMoves2 = moves
+		} else if point < minPoint3 {
+			minPoint3 = point
+			minMoves3 = moves
 		}
 	}
 
 	drop := board[y][x]
-	dropR := board[y][x+1]
-	dropD := board[y+1][x]
-	dropL := board[y][x-1]
-	dropU := board[y-1][x]
+	// 高速化のため。
+	nPlus := n+1
+	nMinus := n-1
+	xPlus := x+1
+	xMinus := x-1
+	yPlus := y+1
+	yMinus := y-1
 
 	// Right
-	if moves[n-1] != "←" && dropR != 9 {
+	if moves[nMinus] != "←" && x != 5 {
+		dropR := board[y][xPlus]
 		board[y][x] = dropR
-		board[y][x+1] = drop
+		board[y][xPlus] = drop
 		moves[n] = "→"
-		move(n+1, board, x+1, y)
+		move(nPlus, board, xPlus, y)
 		board[y][x] = drop
-		board[y][x+1] = dropR
+		board[y][xPlus] = dropR
 		moves[n] = ""
 	}
 
 	// Down
-	if moves[n-1] != "↑" && dropD != 9 {
+	if moves[nMinus] != "↑" && y != 4 {
+		dropD := board[yPlus][x]
 		board[y][x] = dropD
-		board[y+1][x] = drop
+		board[yPlus][x] = drop
 		moves[n] = "↓"
-		move(n+1, board, x, y+1)
+		move(nPlus, board, x, yPlus)
 		board[y][x] = drop
-		board[y+1][x] = dropD
+		board[yPlus][x] = dropD
 		moves[n] = ""
 	}
 
 	// Left
-	if moves[n-1] != "→" && dropL != 9 {
+	if moves[nMinus] != "→" && x != 0 {
+		dropL := board[y][xMinus]
 		board[y][x] = dropL
-		board[y][x-1] = drop
+		board[y][xMinus] = drop
 		moves[n] = "←"
-		move(n+1, board, x-1, y)
+		move(nPlus, board, xMinus, y)
 		board[y][x] = drop
-		board[y][x-1] = dropL
+		board[y][xMinus] = dropL
 		moves[n] = ""
 	}
 
 	// Up
-	if moves[n-1] != "↓" && dropU != 9 {
+	if moves[nMinus] != "↓" && y != 0 {
+		dropU := board[yMinus][x]
 		board[y][x] = dropU
-		board[y-1][x] = drop
+		board[yMinus][x] = drop
 		moves[n] = "↑"
-		move(n+1, board, x, y-1)
+		move(nPlus, board, x, yMinus)
 		board[y][x] = drop
-		board[y-1][x] = dropU
+		board[yMinus][x] = dropU
 		moves[n] = ""
 	}
 }
@@ -188,59 +193,51 @@ func move(n int, board Board, x int, y int) {
 // delete ドロップを消す
 func delete(board Board) (Board, bool) {
 	isWork := false
-	deletedBoard := Board {
-		{9, 9, 9, 9, 9, 9, 9, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 9, 9, 9, 9, 9, 9, 9},
-	}
 
-	for i := 1; i < 6; i++ {
-		for j := 1; j < 7; j++ {
-			deletedBoard[i][j] = board[i][j]
-		}
+	// 非常に汚い書き方だが、多分これの方が高速。
+	deletedBoard := Board {
+		{board[0][0], board[0][1], board[0][2], board[0][3], board[0][4], board[0][5]},
+		{board[1][0], board[1][1], board[1][2], board[1][3], board[1][4], board[1][5]},
+		{board[2][0], board[2][1], board[2][2], board[2][3], board[2][4], board[2][5]},
+		{board[3][0], board[3][1], board[3][2], board[3][3], board[3][4], board[3][5]},
+		{board[4][0], board[4][1], board[4][2], board[4][3], board[4][4], board[4][5]},
 	}
 
 	// 横方向の削除
-	for i := 1; i < 6; i++ {
-		for j := 2; j < 6; j++ {
+	for i := 0; i < 5; i++ {
+		for j := 1; j < 5; j++ {
 			drop := board[i][j]
+			jPlus := j+1
+			jMinus := j-1
 
 			if drop == 0 {
 				continue
 			}
 
-			dropL := board[i][j-1]
-			dropR := board[i][j+1]
-
-			if dropL == drop && drop == dropR {
-				deletedBoard[i][j-1] = 0
+			if board[i][jMinus] == drop && drop == board[i][jPlus] {
+				deletedBoard[i][jMinus] = 0
 				deletedBoard[i][j] = 0
-				deletedBoard[i][j+1] = 0
+				deletedBoard[i][jPlus] = 0
 				isWork = true
 			}
 		}
 	}
 
 	// 縦方向の削除
-	for i := 2; i < 5; i++ {
-		for j := 1; j < 7; j++ {
+	for i := 1; i < 4; i++ {
+		for j := 0; j < 6; j++ {
 			drop := board[i][j]
+			iPlus := i+1
+			iMinus := i-1
 
 			if drop == 0 {
 				continue
 			}
 
-			dropU := board[i-1][j]
-			dropD := board[i+1][j]
-
-			if dropU == drop && drop == dropD {
-				deletedBoard[i-1][j] = 0
+			if board[iMinus][j] == drop && drop == board[iPlus][j] {
+				deletedBoard[iMinus][j] = 0
 				deletedBoard[i][j] = 0
-				deletedBoard[i+1][j] = 0
+				deletedBoard[iPlus][j] = 0
 				isWork = true
 			}
 		}
@@ -252,20 +249,18 @@ func delete(board Board) (Board, bool) {
 // fall ドロップを落とす
 func fall(board Board) Board {
 	fallenBoard := Board {
-		{9, 9, 9, 9, 9, 9, 9, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 0, 0, 0, 0, 0, 0, 9},
-		{9, 9, 9, 9, 9, 9, 9, 9},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
+		{0, 0, 0, 0, 0, 0},
 	}
 
 	// 列ごとにループを回しているということを明示するためにjとしている。
-	for j := 1; j < 7; j++ {
+	for j := 0; j < 6; j++ {
 		// ドロップが落ちる先のy座標、的なニュアンス
-		nextY := 5
-		for i := 5; i > 0; i-- {
+		nextY := 4
+		for i := 4; i >= 0; i-- {
 			drop := board[i][j]
 			if drop != 0 {
 				fallenBoard[nextY][j] = drop
@@ -277,7 +272,7 @@ func fall(board Board) Board {
 	return fallenBoard
 }
 
-// calcPoint どれだけ消えたかを計算
+// calcPoint どれだけ残るかを計算
 func calcPoint(board Board) int {
 	var isWork       bool
 
@@ -291,9 +286,9 @@ func calcPoint(board Board) int {
 		}
 
 		// 消えなくなったらpointを計算
-		for i := 1; i < 6; i++ {
-			for j := 1; j < 7; j++ {
-				if board[i][j] == 0 {
+		for i := 0; i < 5; i++ {
+			for j := 0; j < 6; j++ {
+				if board[i][j] != 0 {
 					point++
 				}
 			}
